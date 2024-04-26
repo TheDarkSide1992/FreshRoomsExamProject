ca85f98a5c436058414fa6d11ecbe61035b61df7
hjorthen1243 <114651351+hjorthen1243@users.noreply.github.com>
2024-04-26 12:24:39 +0200

    frontend begun setup

---
+++ Frontend/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Home', url: '/folder/Home', icon: 'home' },
+    { title: 'Manage', url: '/folder/Manage', icon: 'build' },
+    { title: 'Rooms', url: '/folder/Rooms', icon: 'list' },
+    { title: 'Account', url: '/folder/User', icon: 'person-circle' },
+  ];
+  public labels = ['Living Room', 'Bathroom', 'Bedroom'];
+  constructor() {}
+}

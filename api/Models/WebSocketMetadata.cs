b04f9c8f981e8f542b47995ed812943f59b8eced
andreas14101 <andreasshansen@live.dk>
2024-04-30 09:40:49 +0200

    jwt should work

---
+++ api/Models/WebSocketMetadata.cs
@@ -0,0 +1,11 @@
+﻿using Fleck;
+using Infastructure.DataModels;
+
+namespace api.Models;
+
+public class WebSocketMetadata
+{
+    public IWebSocketConnection? Socket {get; set;}
+    public bool isAuthenticated {get; set;}
+    public User userInfo {get; set;}
+}
---
+++ api/StaticHelpers/ExtentionMethods/WSExtentions.cs
@@ -0,0 +1,23 @@
+﻿using api.Models;
+using api.State;
+using Fleck;
+
+namespace api.StaticHelpers.ExtentionMethods;
+
+public static class WSExtentions
+{
+    public static void AddConnection(this IWebSocketConnection ws)
+    {
+        WebSocketConnections.connections.TryAdd(ws.ConnectionInfo.Id,
+            new WebSocketMetadata
+            {
+                Socket = ws
+            });
+    }
+
+    public static void RemoveFromConnections(this IWebSocketConnection connection)
+    {
+        WebSocketConnections.connections.TryRemove(connection.ConnectionInfo.Id, out _);
+    }
+    
+}
